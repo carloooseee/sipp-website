fc832f919c3b343107d8cbc7a579482510fd86da
Carlos Laran <carloooseeen@gmail.com>
2026-05-22 02:55:32 +0800

    hero cards

--- src/assets/pictures.js
+++ src/assets/pictures.js
@@ -1,14 +1,27 @@
 /** Shared KDBM imagery — import from here to keep usage consistent. */
-export { default as kdbmOverview } from './pictures/kdbm_overview.jpg';
-export { default as pic1 } from './pictures/pic1.jpg';
-export { default as pic2 } from './pictures/pic2.jpg';
-export { default as pic3 } from './pictures/pic3.jpg';
-export { default as pic4 } from './pictures/pic4.jpg';
-export { default as pic5 } from './pictures/pic5.jpg';
-export { default as pic6 } from './pictures/pic6.jpg';
-export { default as pic7 } from './pictures/pic7.jpg';
-export { default as communityBanner } from './22.jpg';
-export { default as loginBg } from './loginsignup.jpg';
+import pic1 from './pictures/pic1.jpg';
+import pic2 from './pictures/pic2.jpg';
+import pic3 from './pictures/pic3.jpg';
+import pic4 from './pictures/pic4.jpg';
+import pic5 from './pictures/pic5.jpg';
+import pic6 from './pictures/pic6.jpg';
+import pic7 from './pictures/pic7.jpg';
+import kdbmOverview from './pictures/kdbm_overview.jpg';
+import communityBanner from './22.jpg';
+import loginBg from './loginsignup.jpg';
+
+export {
+  pic1,
+  pic2,
+  pic3,
+  pic4,
+  pic5,
+  pic6,
+  pic7,
+  kdbmOverview,
+  communityBanner,
+  loginBg,
+};
 
 export const galleryImages = [pic1, pic2, pic3, pic4, pic5, pic6, pic7];
 export const projectImages = [pic3, pic7, pic1, pic5, pic6, pic4, pic2];
